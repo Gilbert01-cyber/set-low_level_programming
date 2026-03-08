1b23e61bf345d91dbe6f93b704cc143212dfa242
Gilbert01-cyber <nggilbert24@gmail.com>
2026-03-08 05:34:31 -0700

    Add task 5 sign

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,3 @@
-
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -7,8 +6,19 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
+int print_sign(int n);
 
 #endif
+
+
+
+
+
+
+
+
+
+
 
 
 
