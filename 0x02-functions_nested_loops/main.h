bb480782502ec916410f07f462a20876a274add8
Gilbert01-cyber <nggilbert24@gmail.com>
2026-03-08 05:21:57 -0700

    Add task 4 isalpha

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,4 @@
+
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -5,8 +6,16 @@ int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
+int _isalpha(int c);
 
 #endif
+
+
+
+
+
+
+
 
 
 
