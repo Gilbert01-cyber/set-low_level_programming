dcfc6919c778a4691574eb0015b291692c242369
Gilbert01-cyber <nggilbert24@gmail.com>
2026-03-22 03:04:04 -0700

    Add main.h and fix Task 3 compilation

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+
+#endif
