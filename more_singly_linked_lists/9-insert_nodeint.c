20a43f8477707436112e125027be9f2866216a5c
Gilbert01-cyber <nggilbert24@gmail.com>
2026-05-31 12:18:48 -0700

    Fix compilation and index logic

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -3,7 +3,7 @@
 /**
  * insert_nodeint_at_index - Inserts a new node at a given position.
  * @head: A pointer to the pointer of the head of the list.
- * @idx: The index where the new node should be added (starts at 0).
+ * @idx: The index where the new node should be added. Index starts at 0.
  * @n: The integer data to store in the new node.
  *
  * Return: The address of the new node, or NULL if it failed.
@@ -12,7 +12,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
 	listint_t *new_node;
-	listint_t *current;
+	listint_t *temp;
 
 	if (head == NULL)
 		return (NULL);
@@ -22,9 +22,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	new_node->n = n;
-	new_node->next = NULL;
 
-	/* Case 1: Insert at the very beginning */
 	if (idx == 0)
 	{
 		new_node->next = *head;
@@ -32,23 +30,20 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new_node);
 	}
 
-	/* Case 2: Traverse to the node right before the target index */
-	current = *head;
-	for (i = 0; current != NULL && i < idx - 1; i++)
+	temp = *head;
+	for (i = 0; temp != NULL && i < idx - 1; i++)
 	{
-		current = current->next;
+		temp = temp->next;
 	}
 
-	/* If index is out of bounds */
-	if (current == NULL)
+	if (temp == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
 
-	/* Link the new node into the list */
-	new_node->next = current->next;
-	current->next = new_node;
+	new_node->next = temp->next;
+	temp->next = new_node;
 
 	return (new_node);
 }
