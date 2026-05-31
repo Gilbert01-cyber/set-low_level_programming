7517a0daa614849bbb029fba0d8bbcdae3cf96d1
Gilbert01-cyber <nggilbert24@gmail.com>
2026-05-31 12:45:40 -0700

    Implement print_listint_safe

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -1,27 +1,25 @@
-#include <stdlib.h>
 #include "lists.h"
 
 /**
  * add_nodeint - adds a new node at the beginning of a listint_t list
- * @head: double pointer to the head of the listint_t list
- * @n: integer to be added to the new node
- *
- * Return: the address of the new element, or NULL if it failed
+ * @head: pointer to pointer of head of list
+ * @n: integer to be added
+ * Return: address of the new element, or NULL if it failed
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new_node;
+	listint_t *new;
 
 	if (head == NULL)
 		return (NULL);
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
 		return (NULL);
 
-	new_node->n = n;
-	new_node->next = *head;
-	*head = new_node;
+	new->n = n;
+	new->next = *head;
+	*head = new;
 
-	return (new_node);
+	return (new);
 }
