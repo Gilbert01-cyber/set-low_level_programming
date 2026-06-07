3a0447eaef7a0db9da83b824a0ee5483ec0d01dd
Gilbert NDAGIJIMANA <nggilbert24@gmail.com>
2026-06-07 21:39:35 +0200

    Add 4-free_dlistint.c file implementation

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_dlistint - Frees a dlistint_t list.
+ * @head: A pointer to the head of the dlistint_t list.
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *next_node;
+
+	while (head != NULL)
+	{
+		next_node = head->next;
+		free(head);
+		head = next_node;
+	}
+}
